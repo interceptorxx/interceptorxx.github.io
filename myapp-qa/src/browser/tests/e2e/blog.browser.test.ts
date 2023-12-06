8163e43dd2112cb05122f6b409629705312311bf
interceptorxx <152772842+interceptorxx@users.noreply.github.com>
2023-12-06 02:55:27 -0800

    Update blog.browser.test.ts

--- myapp-qa/src/browser/tests/e2e/blog.browser.test.ts
+++ myapp-qa/src/browser/tests/e2e/blog.browser.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test('svelte', async ({ page }) => {
+test('Test 2', async ({ page }) => {
     await page.goto('/');
     const h1Text = await page.textContent('h1');
     expect(h1Text).toBe('Domain');
