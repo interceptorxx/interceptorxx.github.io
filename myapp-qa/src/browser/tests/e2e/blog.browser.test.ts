8bc923da596bd526fa1a5940688a98b171b5039c
interceptorxx <152772842+interceptorxx@users.noreply.github.com>
2023-12-06 02:51:19 -0800

    test2

--- myapp-qa/src/browser/tests/e2e/blog.browser.test.ts
+++ myapp-qa/src/browser/tests/e2e/blog.browser.test.ts
@@ -1,3 +1,11 @@
+import { expect, test } from "@playwright/test";
+
+test('svelte', async ({ page }) => {
+    await page.goto('/');
+    const h1Text = await page.textContent('h1');
+    expect(h1Text).toBe('Domain');
+});
+
 // import {test} from '@playwright/test'
 // import {expect, debugFixture} from "@utils/pw-predefine";
 //
